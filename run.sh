7fe3e7377b203f7b75fdd3d19128718b9ae58658
Robert Welch <Robert.Welch@untsystem.edu>
2026-07-18 16:39:19 -0500

    create script to run ansible tasks

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+args="$@"
+stdout=""
+
+error_handler() {
+    local exit_code=$?
+    local line_number=$1
+    echo "Script failed at line $line_number with exit code $exit_code"
+    echo "$stdout"
+}
+
+trap 'error_handler $LINENO' ERR
+
+if [[ ! -d venv ]]; then
+    echo "Create venv..."
+    stdout=$(python3 -m venv venv 2>&1)
+fi
+
+echo "Sourcing venv..."
+stdout=$(source venv/bin/activate 2>&1)
+
+echo "Install python requirements..."
+stdout=$(pip install --quiet --requirement requirements.txt 2>&1)
+
+echo "Install ansible requirements..."
+stdout=$(ansible-galaxy install --role-file requirements.yml 2>&1)
+
+echo "Lint ansible..."
+stdout=$(ansible-lint 2>&1)
+
+echo "Run main.yml$([[ -n "$args" ]] && echo " $args" || true)..."
+ansible-playbook -i ansible_tailscale_inventory.py main.yml $args || true
