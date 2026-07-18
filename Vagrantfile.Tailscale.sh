0108dd5a77a5e871891c0e731f80e72ec187f8b3
Robert Welch <Robert.Welch@untsystem.edu>
2026-07-18 15:06:08 -0500

    new provisioner script to install tailscale

---
+++ Vagrantfile.Tailscale.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+TAILSCALE_AUTHKEY="$1"
+if [[ -n "${TAILSCALE_AUTHKEY}" ]]; then
+    curl -fsSL https://tailscale.com/install.sh | sh 2>&1
+    sudo tailscale up --reset --auth-key=${TAILSCALE_AUTHKEY} --ssh --force-reauth --advertise-tags workstations
+else
+    echo "ERROR: Must provide TAILSCALE_AUTHKEY in ./vagrant/secrets.rb"
+    echo "       View README.md for steps on how to acquire the TAILSCALE_AUTHKEY"
+    exit 1
+fi
